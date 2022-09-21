9c3cf1a91493538f32efd47c722bb142303ad930
Ben <benedict.jude.rocat@gmail.com>
2022-09-21 14:20:37 +0800

    🚧 Various testing

--- commuter/src/pages/IndexPage.vue
+++ commuter/src/pages/IndexPage.vue
@@ -1,42 +1,71 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <div class="text-h6">Map</div>
+    <p></p>
+    <div
+      ref="mapEl"
+      id="map"
+      class="h-[60vh] w-[calc(100vw-100px)] md:w-96 rounded-lg border border-medrx"
+    />
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-import { ref } from 'vue';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { watch, ref } from 'vue';
+import { useQuasar } from 'quasar';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-const meta = ref<Meta>({
-  totalCount: 1200
+const $q = useQuasar();
+
+if ($q.platform.is.capacitor) {
+  $q.notify({ message: 'developer: Platform unsupported' });
+} else if ($q.platform.is.desktop || $q.platform.is.mobile) {
+  console.log('Hooking into navigator geolocation API');
+  const Geolocation = navigator.geolocation;
+
+  Geolocation.getCurrentPosition(
+    (position) => {
+      $q.notify({ message: `success! position is ${position}` });
+    },
+    (err) => {
+      $q.notify({ message: `failed: ${err}` });
+    }
+  );
+}
+
+const mapEl = ref<HTMLElement | null>(null);
+let map: L.Map | null = null;
+
+watch(mapEl, (newMapEl) => {
+  if (!newMapEl) return;
+  map = L.map('map').setView([0.5, 0.5], 13);
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
+
+  // API example:
+  // const m = L.marker([
+  //   props.initialCoordinates.lat,
+  //   props.initialCoordinates.lng,
+  // ])
+  //   .addTo(map)
+  //   .setIcon(
+  //     L.icon({
+  //       iconUrl:
+  //         'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+  //       shadowUrl:
+  //         'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  //       iconSize: [25, 41],
+  //       iconAnchor: [12, 41],
+  //       popupAnchor: [1, -34],
+  //       shadowSize: [41, 41],
+  //     })
+  //   );
+
+  // Example: update pin on map move
+  // map.on('move', () => m.setLatLng(map.getCenter()));
+  // map.on('zoomanim', () => m.setLatLng(map.getCenter()));
 });
 </script>
