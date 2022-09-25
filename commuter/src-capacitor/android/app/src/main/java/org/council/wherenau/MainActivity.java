338d48bb39e9a4a3ea75cae36a97e901d876025f
Ben <benedict.jude.rocat@gmail.com>
2022-09-25 18:48:04 +0800

    💄 Setting navigation bar & status bar transparent

--- commuter/src-capacitor/android/app/src/main/java/org/council/wherenau/MainActivity.java
+++ commuter/src-capacitor/android/app/src/main/java/org/council/wherenau/MainActivity.java
@@ -1,5 +1,27 @@
 package org.council.wherenau;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+    View decorView = this.getWindow().getDecorView();
+    int uiOptions = decorView.getSystemUiVisibility();
+    uiOptions = uiOptions | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+    decorView.setSystemUiVisibility(uiOptions);
+    this.getWindow().setStatusBarColor(Color.TRANSPARENT);
+
+    Log.d("Debug", "Main activity run!");
+  }
+}
